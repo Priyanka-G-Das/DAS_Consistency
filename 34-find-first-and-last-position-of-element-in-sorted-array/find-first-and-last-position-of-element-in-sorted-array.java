3674482622fa0d079fc21cfa74be97c3a2e3cc03
Priyanka_Das <57655916+Priyanka-G-Das@users.noreply.github.com>
2025-02-12 23:38:24 +0530

    Time: 0 ms (100.00%) | Memory: 45.8 MB (82.98%) - LeetSync

---
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -0,0 +1,62 @@
+class Solution {
+    /*
+     * 1st pos
+     * nums[i] >= target
+     * f f f f t t t t
+     * find 1st occ of true
+     * 
+     * last pos
+     * nums[i] <= target
+     * t t t t f f f f
+     * find last occ of true
+     */
+    // T.C - O(logN), S.C - O(1)
+    public int[] searchRange(int[] nums, int target) {
+        int low = 0;
+        int ans1 = -1;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (check1(nums, mid, target)) {
+                ans1 = mid;
+                high = mid - 1;
+
+            } else {
+                low = mid + 1;
+            }
+
+        }
+
+        if (ans1 == -1 || nums[ans1] != target)
+            return new int[] { -1, -1 };
+
+        int ans2 = -1;
+        int low2 = 0;
+        int high2 = nums.length - 1;
+        while (low2 <= high2) {
+            int mid = low2 + (high2 - low2) / 2;
+            if (check2(nums, mid, target)) {
+                ans2 = mid;
+                low2 = mid + 1;
+
+            } else {
+
+                high2 = mid - 1;
+            }
+
+        }
+        return new int[] { ans1, ans2 };
+
+    }
+
+    // this function just to return the index value of targetrujjuu
+    public boolean check1(int nums[], int mid, int target) {
+        return nums[mid] >= target;
+
+    }
+
+    public boolean check2(int nums[], int mid, int target) {
+        return nums[mid] <= target;
+
+    }
+}
